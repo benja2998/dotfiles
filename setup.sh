6908239158d05d99b18ff55a624a7519480bddfd
benja2998 <benja2998@noreply.codeberg.org>
2025-05-25 17:56:04 -0400

    update

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 cp ./.config/fish/config.fish ~/.config/fish/config.fish
 cp ./.config/neofetch/config.conf ~/.config/neofetch/config.conf
 cp ./.config/starship.toml ~/.config/starship.toml
+cp ./.alias ~/.alias
 echo "Setup complete"
