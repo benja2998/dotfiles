3ef7f81ea5a8275ef13794ea969278f4972abe6c
benja2998 <benja2998@duck.com>
2025-11-10 00:46:16 -0300

    Add nvim-tree
    Signed-off-by: benja2998 <benja2998@duck.com>

--- neovim/init.lua
+++ neovim/init.lua
@@ -13,9 +13,13 @@ vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
 vim.keymap.set("n", "<leader>c", ":tabclose<CR>")
 vim.keymap.set("n", "<leader>n", ":tabnew<CR>")
-vim.keymap.set("n", "<leader>e", ":Ex %:p:h<CR>")
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<S-Tab>", ":tabprevious<CR>")
 vim.keymap.set("n", "<Tab>", ":tabnext<CR>")
+vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -29,8 +33,29 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { "nvim-lualine/lualine.nvim" },
+  { "nvim-tree/nvim-tree.lua" },
   { "nvim-telescope/telescope.nvim" },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+})
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.termguicolors = true
+
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
 })
 
 require("nvim-treesitter.configs").setup({
@@ -68,12 +93,6 @@ require("lualine").setup({
 
 require("catppuccin").setup({ flavour = "mocha" })
 vim.cmd.colorscheme("catppuccin-mocha")
-
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 0
-vim.g.netrw_winsize = 25
-vim.g.netrw_keepdir = 0
 
 vim.keymap.set("n", "<leader>t", function()
   local dir = vim.fn.expand("%:p:h")
