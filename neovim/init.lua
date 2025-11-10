c9f8fa1cf1094cf0f0b8128c287d272c9d532290
benja2998 <benja2998@duck.com>
2025-11-10 17:32:14 -0300

    Update
    Signed-off-by: benja2998 <benja2998@duck.com>

--- neovim/init.lua
+++ neovim/init.lua
@@ -3,19 +3,20 @@ vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-vim.g.mapleader = ","
 vim.o.cursorline = true
+vim.g.mapleader = ','
+vim.o.clipboard = 'unnamedplus'
 
 vim.api.nvim_set_keymap('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>h", ":noh<CR>")
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.keymap.set("n", "<leader>q", ":q<CR>")
-vim.keymap.set("n", "<leader>c", ":tabclose<CR>")
-vim.keymap.set("n", "<leader>n", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
-vim.keymap.set("n", "<S-Tab>", ":tabprevious<CR>")
-vim.keymap.set("n", "<Tab>", ":tabnext<CR>")
+vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { silent = true })
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>c", ":bdelete<CR>", { silent = true })
+vim.keymap.set("n", "<leader>n", ":enew<CR>", { silent = true })
 vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
@@ -23,20 +24,54 @@ vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
-  })
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  { "nvim-lualine/lualine.nvim" },
-  { "nvim-tree/nvim-tree.lua" },
-  { "nvim-telescope/telescope.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "neovim/nvim-lspconfig" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-lualine/lualine.nvim" },
+    { "nvim-tree/nvim-tree.lua" },
+    { "nvim-telescope/telescope.nvim" },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 })
+
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" }
+            }
+        }
+    }
+})
+vim.lsp.config("pyright", {})
+vim.lsp.config("ts_ls", {})
+vim.lsp.config("clangd", {})
+vim.lsp.enable("clangd")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("pyright")
+vim.lsp.enable("ts_ls")
+
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+})
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -44,38 +79,38 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    sort = {
+        sorter = "case_sensitive",
+    },
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
 })
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "python", "javascript" },
-  highlight = { enable = true },
+    ensure_installed = { "lua", "python", "javascript" },
+    highlight = { enable = true },
 })
 
 require('telescope').setup {
-  defaults = {
-    hidden = true,
-    mappings = {
-      i = { ["<C-u>"] = false, ["<C-d>"] = false },
+    defaults = {
+        hidden = true,
+        mappings = {
+            i = { ["<C-u>"] = false, ["<C-d>"] = false },
+        },
     },
-  },
-  pickers = {
-    find_files = {
-      hidden = true,
-      no_ignore = true,
+    pickers = {
+        find_files = {
+            hidden = true,
+            no_ignore = true,
+        }
     }
-  }
 }
 
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
@@ -84,18 +119,59 @@ vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
 
 require("lualine").setup({
-  options = {
-    theme = "auto",
-    section_separators = "",
-    component_separators = ""
-  },
+    options = {
+        theme = "catppuccin",
+        icons_enabled = true,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        disabled_filetypes = { "NvimTree", "lazy" },
+        always_divide_middle = true,
+    },
+    sections = {
+        lualine_a = { { "mode", icon = "" } },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { { "location", icon = "" } },
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+    },
+    tabline = {},
+    extensions = { "nvim-tree", "quickfix", "fugitive" },
+})
+
+require("bufferline").setup({
+    options = {
+        mode = "buffers",
+        numbers = "none",
+        diagnostics = "nvim_lsp",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        separator_style = "slant",
+        always_show_bufferline = true,
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                highlight = "Directory",
+                separator = true
+            }
+        },
+    },
 })
 
 require("catppuccin").setup({ flavour = "mocha" })
 vim.cmd.colorscheme("catppuccin-mocha")
 
 vim.keymap.set("n", "<leader>t", function()
-  local dir = vim.fn.expand("%:p:h")
-  vim.cmd("lcd " .. dir)
-  vim.cmd("terminal")
+    local dir = vim.fn.expand("%:p:h")
+    vim.cmd("lcd " .. dir)
+    vim.cmd("terminal")
 end)
