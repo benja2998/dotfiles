184d6525a67f2e61a3c278d3d221252b73607866
benja2998 <benja2998@duck.com>
2025-12-26 18:55:55 -0300

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,6 +21,26 @@ require("lazy").setup({
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
 
+    -- Tmux integration
+    {
+        "benja2998/vim-tmux-navigator", -- My fork of vim-tmux-navigator
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+            "TmuxNavigatorProcessList",
+        },
+        keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
+
     -- File manager
     "stevearc/oil.nvim",
 
