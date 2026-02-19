62e46543f68b2bda70268ad2beece07330181c31
benja2998 <benja2998@duck.com>
2026-02-19 13:43:34 -0300

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    set -gx PATH $HOME/.cargo/bin $HOME/.local/bin $PATH
+    set -gx TERM "xterm-256color"
+    set -gx COLORTERM "truecolor"
+    set -gx EDITOR 'emacsclient -t -a ""'
+    set -gx VISUAL 'emacsclient -c -a ""'
+    set -gx HISTSIZE 50000
+
+    alias ls 'ls --color=auto'
+    alias dir 'dir --color=auto'
+    alias tree 'tree -C'
+    alias emacs 'emacsclient -c -a ""'
+    alias grep 'grep --color=auto'
+
+    if test -x /opt/homebrew/bin/brew
+        /opt/homebrew/bin/brew shellenv | source
+    else if test -x /usr/local/bin/brew
+        /usr/local/bin/brew shellenv | source
+    else if test -x /home/linuxbrew/.linuxbrew/bin/brew
+        /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
+    end
+
+    zoxide init --cmd cd fish | source
+end
