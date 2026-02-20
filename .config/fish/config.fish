a99d32d532b1a2b7a3fd7770634d3cec7262b47e
benja2998 <benja2998@duck.com>
2026-02-20 16:30:05 -0300

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,12 @@
 if status is-interactive
     set -gx PATH $HOME/.cargo/bin $HOME/.local/bin $PATH
-    set -gx TERM "xterm-256color"
-    set -gx COLORTERM "truecolor"
     set -gx EDITOR 'emacsclient -t -a ""'
     set -gx VISUAL 'emacsclient -c -a ""'
 
-    alias ls 'ls --color=auto'
-    alias dir 'dir --color=auto'
-    alias tree 'tree -C'
     alias emacs 'emacsclient -c -a ""'
     alias rm 'rm -i'
     alias mv 'mv -i'
     alias cp 'cp -i'
-    alias grep 'grep --color=auto'
 
     if test -x /opt/homebrew/bin/brew
         /opt/homebrew/bin/brew shellenv | source
