1f529aefa769b27992a7309a59881c1f59741fb5
benja2998 <benja2998@duck.com>
2026-02-24 18:11:17 -0300

    update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,15 @@
-;; Use Org-Babel to load the configuration from config.org
-(org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+(setq vc-follow-symlinks t)
+
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'after-init-hook
+  (lambda () (setq gc-cons-threshold (* 16 1024 1024))))
+
+(let ((org-file "~/.emacs.d/config.org")
+      (el-file "~/.emacs.d/config.el")
+      (elc-file "~/.emacs.d/config.elc"))
+  (when (or (not (file-exists-p elc-file))
+            (file-newer-than-file-p org-file elc-file))
+    (require 'org)
+    (org-babel-tangle-file org-file el-file "elisp")
+    (byte-compile-file el-file))
+  (load-file elc-file))
