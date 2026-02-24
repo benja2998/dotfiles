29723265094dc7dc947f9544e1683dec0cb6dfd0
benja2998 <benja2998@duck.com>
2026-02-24 18:53:11 -0300

    update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,7 +1,7 @@
 (setq vc-follow-symlinks t)
 
 (setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'after-init-hook
+(add-hook 'emacs-startup-hook
   (lambda () (setq gc-cons-threshold (* 16 1024 1024))))
 
 (let ((org-file "~/.emacs.d/config.org")
