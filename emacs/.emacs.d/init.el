14cacd5b4b055c2594699ada015d1a4d48d91b4f
benja2998 <benja2998@duck.com>
2026-03-02 21:31:24 -0300

    update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,11 +24,6 @@
 ;;; Font
 (cond ((eq system-type 'android) (set-face-attribute 'default nil :font "Droid Sans Mono-18")) ((eq system-type 'gnu/linux) (set-face-attribute 'default nil :font "Iosevka-20")) ((eq system-type 'darwin) (set-face-attribute 'default nil :font "Iosevka-20")) (t (set-face-attribute 'default nil :font "Monospace-12")))
 
-;;; Enable toolbar, menubar and modifier bar if on Android
-(cond ((eq system-type 'android) (tool-bar-mode 1)))
-(cond ((eq system-type 'android) (modifier-bar-mode 1)))
-(cond ((eq system-type 'android) (menu-bar-mode 1)))
-
 ;;; Theme
 (load-theme 'modus-vivendi t)
 
