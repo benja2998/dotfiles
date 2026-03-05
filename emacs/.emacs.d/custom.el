a1f230512ad728c2f38301717e0c98e35347b495
benja2998 <benja2998@duck.com>
2026-03-05 00:28:29 -0300

    update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,11 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(dracula))
+ '(custom-enabled-themes '(modus-vivendi))
  '(custom-safe-themes
    '("9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
      default))
- '(package-selected-packages '(dracula-theme eat magit)))
+ '(package-selected-packages '(eat magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
