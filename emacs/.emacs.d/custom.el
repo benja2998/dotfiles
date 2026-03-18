44f6372d91b3071bb2d1ad2d599fc4e8c9129eb3
benja2998 <benja2998@duck.com>
2026-03-18 13:38:05 -0300

    update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(dracula))
+ '(custom-enabled-themes '(modus-vivendi))
  '(custom-safe-themes
    '("9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
      default)))
