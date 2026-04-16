ebd14d31a9a5395a32e8b3ee116d88ef4c8b386e
benja2998 <benja2998@duck.com>
2026-04-16 16:47:14 -0400

    update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -7,9 +7,14 @@
  '(auto-save-default nil)
  '(c-basic-offset 4)
  '(create-lockfiles nil)
- '(custom-enabled-themes '(modus-vivendi))
+ '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
+   '("b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d"
+     "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20"
+     "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe"
+     "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7"
+     "e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     "9c6aa7eb1bde73ba1142041e628827492bd05678df4d9097cda21b1ebcb8f8b9"
      default))
  '(display-line-numbers-type 'relative)
  '(fido-mode t)
@@ -20,7 +25,7 @@
  '(package-archives
    '(("elpa" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
- '(package-selected-packages '(exec-path-from-shell))
+ '(package-selected-packages '(exec-path-from-shell gruber-darker-theme))
  '(pixel-scroll-precision-mode t)
  '(prog-mode-hook '(#[nil ((display-line-numbers-mode 1)) nil]))
  '(savehist-mode t)
