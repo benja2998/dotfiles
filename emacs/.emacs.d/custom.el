3fd2dd4ab2b2077531094c0c332bd5e30d701985
benja2998 <benja2998@duck.com>
2026-04-16 17:01:20 -0400

    update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -7,9 +7,27 @@
  '(auto-save-default nil)
  '(c-basic-offset 4)
  '(create-lockfiles nil)
- '(custom-enabled-themes '(modus-vivendi))
+ '(custom-enabled-themes '(ef-autumn))
  '(custom-safe-themes
-   '("6dcf1ca4c7432773084b9d52649ee5eb2c663131c4c06859f648dea98d9acb3e"
+   '("0f738dce3f831b6d64ee3e98052bdea663b74d5149dcbbf555327dcb4517fc08"
+     "ed2efc874021b54144b9ed9874e4d9c036090488aeabe19c9ac28576a162ec12"
+     "51caf9bf88aba940d98c96add138d83317d50eae4b8526612184e93473252d54"
+     "677e3d3d9ca354bfa2422287bc8ef048a759cd5009493e75197d3a3ab3590c58"
+     "4515e88dea04cac581b05b1893f461169fad58a1bd1f255605e0e586a354f7bc"
+     "5db7ff8db7fe64558355dba6314cb73008eb7cb1a4ca4552d69fbe588ef4c224"
+     "6a95b0faf6cee6adfda34cdfadb2fed6f4157a1d49aabef8cc9b94c187d69a1d"
+     "dc8d63cd0514d058bdf34774163709834519350c1a80dc04deaa58085a8810a5"
+     "6aff83667a3bad8db2f55d69cf595f58b56af233f116ec24d93fe2aa51033eef"
+     "03ffccc093c553a238a54fea13f2056749d83c24e65940f8d4bdb7135f1199a5"
+     "6965a903ced31bd58caddb7e7035aadc47f8b0a5c57f246b698be2dfdfed2c4e"
+     "be2cf7aada403c8ce552e29fce64b1cda28db4cdc72c1152a6e8989c8ac69847"
+     "76829ecd2a3810c7a20b4163c8b3dbc8c4faf1674bb568e5150dd8d24fef25bf"
+     "f0ddff48ec78dec9ac9ea3236e7208ef58a86d445207a674523c0eba8e9743b0"
+     "749a7bb14efeb8b6c9b251c7a771ab7de500b247eb35f69bfccbdfca27e0602c"
+     "e684e71b7276a85c0ec8ee8f3ff4bc6b61dbc1694288ad049e062029fc072e54"
+     "0da9f48243b77c83719c569efd88a7e47c1d8a3f2887838408745d2d52fad8a8"
+     "01bb579ed8a5b4a77490798626a96ae153288b8c2709e291162cfcc2e0aad647"
+     "6dcf1ca4c7432773084b9d52649ee5eb2c663131c4c06859f648dea98d9acb3e"
      "2c7dc80264de0ba9409d4ebb3c7b31cf8e4982015066174c786f16a672db71b2"
      "516ec39655c85f346393f5d93e0f03602b6bfc33335bf2fd673016c9c4cdc69e"
      "22faff66975354a3dd145ae13934758d45930ad09cbbe8bd1ed74224e4aa5684"
@@ -32,7 +50,7 @@
  '(package-archives
    '(("elpa" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
- '(package-selected-packages '(exec-path-from-shell))
+ '(package-selected-packages '(ef-themes exec-path-from-shell))
  '(pixel-scroll-precision-mode t)
  '(prog-mode-hook '(#[nil ((display-line-numbers-mode 1)) nil]))
  '(savehist-mode t)
