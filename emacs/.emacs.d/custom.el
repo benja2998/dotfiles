dffb2c042e3a6f219531185f21b125be3b6152a0
benja2998 <benja2998@duck.com>
2026-04-17 18:17:16 -0400

    update

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -7,9 +7,35 @@
  '(auto-save-default nil)
  '(c-basic-offset 4)
  '(create-lockfiles nil)
- '(custom-enabled-themes '(modus-vivendi-tinted))
+ '(custom-enabled-themes '(gruber-darker))
  '(custom-safe-themes
-   '("fff0dc54ff5a194ba6593d1cce0fbb4fe8cf9da59fcef47f9e06dec6ef11b1fa"
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     "2505c0699e53e5ff10708b6d86f2224abd79357b27de79136605b450a1785585"
+     "9819b94dc38ee935362d4bf1c1198ebb8ebeabb5e8c85a78436565720d91c0d7"
+     "8a90181e138c25f51a5c001a9c763a1e263605c0654df6cc9546a85e5e0f789d"
+     "1eaf5b063f0a33436f094ac9866a05d7bc8488acdf661110516f9a796d3471af"
+     "ee10ae769b2f1383371a1e271ee1ddbed2bd9969331cc9417921f4251efbce0f"
+     "106fadeab4fb8cf50eeae1e1fd051ae00d0e71a40b5b0f0f5b9db85015398a61"
+     "b3440bf57d65eeb0cce0c45f91a14b336fe5d2d33d15782c701fa092fa34bf59"
+     "366f7fb70999d739ff559568b011f8cfb05c88124e4c944e5e1008312e572137"
+     "d422edfdab7b5f84fb305732eaa36963b890acbd5cc88233309c042b8e967197"
+     "f06a77b1c3433502f0f1db69c0b7a4c686c78d43d7cbfd486ca39d8da983f832"
+     "eccbc5afab954d8d4cd7748921cc93c2e770eb8059cb43bf2ef3bcbb4a9aca51"
+     "c380d4b3988e8fd933d087cbe0b56412e5f8dc7b9712f45c7300bdb2d2e2df5b"
+     "6b9fbe5d88424ac7283b8f36b6f184d1140fcd4bfcab1f72a3c58c48dc254bae"
+     "26a8e9035bda6d46d95bbf224ac52e90eb5befd80f5a083b72865697c8a81304"
+     "1fa665965395c909dc60fc35b66a2ad902904334cf2b9beb2b07dddab67c782a"
+     "b5beaec9ea4c8d0b3a104b4bf9ce4e7519a4a5a045527af8e6a93df85111048c"
+     "4bc34187baf114f1f3de085ffe9510b3c43fe505d21bd52d75bd5aade8c7839e"
+     "ee3ae3d3239725a87ba0caf74f8f4432e19b8f9ab440d716268d86a628232b5d"
+     "00b98b7447d11b4c3bc37588014459a764e8fb3ad3504443af69ead9a5dbc27d"
+     "b5ac0a656be7115492b8a1e39eabac55c2f0fb267577f7f5406278a7ed1198c5"
+     "d8541a7c1ca4117b056b34ddf9ed5a6e2547eaf554dc0e7f7b6e15ed14490a37"
+     "318739fb14250166fa9bc8a64d34f5675eaafe9c5a659bdf8fec5863795ac0eb"
+     "7e98dc1aa7f5db0557691da690c38d55e83ddd33c6d268205d66e430d57fb982"
+     "317f9fc9b30ce2d8064965f1a3cf8645ee3e8dc26dfcdc9f0e2ead337f37cac3"
+     "6dcf1ca4c7432773084b9d52649ee5eb2c663131c4c06859f648dea98d9acb3e"
+     "fff0dc54ff5a194ba6593d1cce0fbb4fe8cf9da59fcef47f9e06dec6ef11b1fa"
      default))
  '(display-line-numbers-type 'relative)
  '(fido-mode t)
@@ -20,7 +46,7 @@
  '(package-archives
    '(("elpa" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
- '(package-selected-packages '(exec-path-from-shell))
+ '(package-selected-packages '(exec-path-from-shell gruber-darker-theme))
  '(pixel-scroll-precision-mode t)
  '(prog-mode-hook '(#[nil ((display-line-numbers-mode 1)) nil]))
  '(savehist-mode t)
