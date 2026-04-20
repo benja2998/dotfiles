9d77fd1462311c99276219c10e18825b3c86d1f6
benja2998 <benja2998@duck.com>
2026-04-20 16:52:36 -0400

    feat: simplify emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,14 +5,8 @@
 ;;; Open home dir with a keybind
 (global-set-key (kbd "C-c h") (lambda () (interactive) (dired "~")))
 
-;;; Fix gpg
-(setq epa-pinentry-mode 'loopback)
-
 ;;; Terminal
-(global-set-key (kbd "C-c t") (lambda () (interactive) (start-process-shell-command "alacritty" nil "alacritty")))
-
-;;; Set the custom-file
-(setq custom-file "~/.emacs.d/custom.el")
+(global-set-key (kbd "C-c t") (lambda () (interactive) (start-process-shell-command "x-terminal-emulator" nil "x-terminal-emulator")))
 
 ;;; Font
 (cond ((eq system-type 'android) (set-face-attribute 'default nil :font "Droid Sans Mono-18")) ((eq system-type 'gnu/linux) (set-face-attribute 'default nil :font "Iosevka-14")) (t (set-face-attribute 'default nil :font "Monospace-12")))
@@ -29,21 +23,38 @@
 (setq inhibit-splash-screen t)
 
 ;;; Fix indentation
+(setq indent-tabs-mode nil)
+(setq tab-width 4)
+(setq c-basic-offset 4)
+
 (add-hook 'makefile-mode-hook
           (lambda ()
             (setq indent-tabs-mode t)
             (setq tab-width 8)))
 
-;;; Load the custom-file
-(load custom-file)
+;;; Line numbers
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+;;; Clean up UI
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+;;; Pixel scrolling
+(pixel-scroll-precision-mode t)
+
+;;; Follow vc symlinks
+(setq vc-follow-symlinks t)
 
 ;;; Ido mode
-(ido-mode 1)
-(ido-everywhere 1)
+(ido-mode t)
+(ido-everywhere t)
 
 ;;; Simple C mode
 (require 'simpc-mode)
 (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
 
 ;;; Gruber darker
+(setq custom-safe-themes t)
 (load-theme 'gruber-darker)
