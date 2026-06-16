72a88541e2e07989f0f22c62db8ea446b91ca88a
benja2998 <benja2998@proton.me>
2026-06-15 20:09:59 -0400

    update

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -1,10 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	if status is-login
+		if test -z "$DISPLAY"; and test (tty) = /dev/tty1
+			exec startx
+		end
+	end	
 	set -gx EDITOR 'emacsclient -c -a ""'
 	set -gx GPG_TTY $(tty)
 	function emacs
-	emacsclient -c -a "" $argv		 
-	end
+		emacsclient -c -a "" $argv		 
+	end	
 
 	starship init fish | source
 end
