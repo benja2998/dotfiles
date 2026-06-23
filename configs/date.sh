9d06c9605be284e632295061409333ce11e65117
benja2998 <benja2998@proton.me>
2026-06-22 23:41:10 -0400

    fix indentation

--- configs/date.sh
+++ configs/date.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 while true; do
-        cmd="󰜟 $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')"
-        if [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ]; then
-                cmd="󰓄 muted"
-        fi
-        echo "$cmd | $(date +'%a %b %d %H:%M') | BAT: $(acpi -b | awk '{print $4}')"
-        sleep 0.1
+    cmd="󰜟 $(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')"
+    if [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ]; then
+        cmd="󰓄 muted"
+    fi
+    echo "$cmd | $(date +'%a %b %d %H:%M') | BAT: $(acpi -b | awk '{print $4}')"
+    sleep 0.1
 done
