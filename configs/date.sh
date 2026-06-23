c804219590a1ca30f0f110eead345791625f7c1b
benja2998 <benja2998@proton.me>
2026-06-23 19:45:15 -0400

    better status bar

--- configs/date.sh
+++ configs/date.sh
@@ -5,6 +5,24 @@ while true; do
 	if [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ]; then
 		cmd="󰓄 muted"
 	fi
-	echo "$cmd | $(date +'%a %b %d %H:%M') | BAT: $(acpi -b | awk '{print $4}')"
+	cmd2=" $(acpi -b | awk '{print $4}')"
+
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 75; then
+		cmd2=" $(acpi -b | awk '{print $4}')"		
+	fi
+
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 50; then
+		cmd2=" $(acpi -b | awk '{print $4}')"		
+	fi
+
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 25; then
+		cmd2=" $(acpi -b | awk '{print $4}')"		
+	fi
+
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 10; then
+		cmd2=" $(acpi -b | awk '{print $4}')"		
+	fi
+	
+	echo "$cmd   $(date +'%a %b %d %H:%M')  $cmd2  "
 	sleep 0.1
 done
