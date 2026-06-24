b67c32982789397fa0800186383cd004986efb4d
benja2998 <benja2998@proton.me>
2026-06-23 23:46:43 -0400

    update

--- configs/date.sh
+++ configs/date.sh
@@ -5,22 +5,22 @@ while true; do
 	if [ "$(pactl get-sink-mute @DEFAULT_SINK@)" = "Mute: yes" ]; then
 		cmd="󰓄 muted"
 	fi
-	cmd2=" $(acpi -b | awk '{print $4}')"
+	cmd2=" $(acpi -b | awk '{print $4}' | sed 's/,//g')"
 
-	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 75; then
-		cmd2=" $(acpi -b | awk '{print $4}')"		
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%,//g')" -le 75; then
+		cmd2=" $(acpi -b | awk '{print $4}' | sed 's/,//g')"		
 	fi
 
-	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 50; then
-		cmd2=" $(acpi -b | awk '{print $4}')"		
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%,//g')" -le 50; then
+		cmd2=" $(acpi -b | awk '{print $4}' | sed 's/,//g')"		
 	fi
 
-	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 25; then
-		cmd2=" $(acpi -b | awk '{print $4}')"		
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%,//g')" -le 25; then
+		cmd2=" $(acpi -b | awk '{print $4}' | sed 's/,//g')"		
 	fi
 
-	if test "$(acpi -b | awk '{print $4}' | sed 's/%//g')" -le 10; then
-		cmd2=" $(acpi -b | awk '{print $4}')"		
+	if test "$(acpi -b | awk '{print $4}' | sed 's/%,//g')" -le 10; then
+		cmd2=" $(acpi -b | awk '{print $4}' | sed 's/,//g')"		
 	fi
 	
 	echo "$cmd   $(date +'%a %b %d %H:%M')  $cmd2  "
