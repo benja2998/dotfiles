b85376549fb6ac74d05cba86bac0a286b193984f
benja2998 <benja2998@proton.me>
2026-07-09 18:02:08 -0400

    update

--- configs/dmenu-music.sh
+++ configs/dmenu-music.sh
@@ -8,4 +8,4 @@ else
 	result=$(printf '%s\n' "${files[@]}" | dmenu -l 10 -fn "Terminess Nerd Font 16" -nb "#24273a" -nf "#cad3f5" -sb "#8aadf4" -sf "#24273a")
 fi
 
-[ -n "$result" ] && mpv "$result"
+[ -n "$result" ] && mpv --script=/usr/lib/mpv-mpris/mpris.so "$result"
