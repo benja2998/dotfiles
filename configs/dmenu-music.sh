e77dc993e1a8fe8144bf5161cd1eef536aeb8d2e
benja2998 <benja2998@proton.me>
2026-07-10 00:28:18 -0400

    use correct colors

--- configs/dmenu-music.sh
+++ configs/dmenu-music.sh
@@ -3,9 +3,9 @@ files=(~/Music/*.opus)
 [ -e "${files[0]}" ] || exit 0
 
 if [ "${XDG_SESSION_TYPE}" = "wayland" ] || [ -n "${WAYLAND_DISPLAY}" ]; then
-	result=$(printf '%s\n' "${files[@]}" | wmenu -l 10 -f "Terminess Nerd Font 16" -N "#24273a" -n "#cad3f5" -S "#8aadf4" -s "#24273a")
+	result=$(printf '%s\n' "${files[@]}" | wmenu -l 10 -f "Terminess Nerd Font 16" -N "#303446" -n "#c6d0f5" -S "#8caaee" -s "#303446")
 else
-	result=$(printf '%s\n' "${files[@]}" | dmenu -l 10 -fn "Terminess Nerd Font 16" -nb "#24273a" -nf "#cad3f5" -sb "#8aadf4" -sf "#24273a")
+	result=$(printf '%s\n' "${files[@]}" | dmenu -l 10 -fn "Terminess Nerd Font 16" -nb "#303446" -nf "#c6d0f5" -sb "#8caaee" -sf "#303446")
 fi
 
 [ -n "$result" ] && mpv --script=/usr/lib/mpv-mpris/mpris.so "$result"
