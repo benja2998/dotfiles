cafc46d95a81dc985c26dcb5347c8c7d4b89b4f9
benja2998 <benja2998@proton.me>
2026-07-16 20:52:48 -0400

    update

--- configs/pipewire.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pkill -u "${USER}" -x pipewire\|wireplumber 1>/dev/null 2>&1
-/usr/bin/pipewire &
-pkill -u "$(USER)" -x pipewire-media-session 1>/dev/null 2>&1
-/usr/bin/pipewire -c pipewire-pulse.conf &
-exec /usr/bin/wireplumber &
