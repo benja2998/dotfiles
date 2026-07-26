df4d415f056587e4cbb39fd1db8943024f87f30e
benja2998 <benja2998@proton.me>
2026-07-26 17:28:31 -0400

    update

--- configs/dmenu-music.sh
+++ configs/dmenu-music.sh
@@ -7,7 +7,7 @@ files=(~/Music/*.{mp3,flac,opus,m4a,aac,wav,ogg,oga,alac})
 if [ "${XDG_SESSION_TYPE}" = "wayland" ] || [ -n "${WAYLAND_DISPLAY}" ]; then
     result=$(printf '%s\n' "${files[@]}" | wmenu -i -l 20 -f "Terminess Nerd Font:size=16" -N "#202326" -n "#fcfcfc" -S "#ff0000" -s "#fcfcfc")
 else
-    result=$(printf '%s\n' "${files[@]}" | dmenu -i -l 20 -fn "Terminess Nerd Font:size=16" -nb "#202326" -nf "#fcfcfc" -sb "#ff0000" -sf "#fcfcfc")
+    result=$(printf '%s\n' "${files[@]}" | dmenu -i -l 20 -fn "Terminess Nerd Font:size=16")
 fi
 
 [ -n "$result" ] && mpv --script=/usr/lib/mpv-mpris/mpris.so "$result"
--- configs/emacs.sh
+++ configs/emacs.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pkill -u "${USER}" -x -f "emacs --daemon" >/dev/null 2>&1
-emacs --daemon
+
