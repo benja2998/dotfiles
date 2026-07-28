3f9e1a0c7ad212a7ec0d30819a1af9e53938a13c
benja2998 <benja2998@proton.me>
2026-07-27 22:28:25 -0400

    update

--- configs/dmenu-music.sh
+++ configs/dmenu-music.sh
@@ -5,9 +5,9 @@ files=(~/Music/*.{mp3,flac,opus,m4a,aac,wav,ogg,oga,alac})
 [ ${#files[@]} -gt 0 ] || exit 0
 
 if [ "${XDG_SESSION_TYPE}" = "wayland" ] || [ -n "${WAYLAND_DISPLAY}" ]; then
-    result=$(printf '%s\n' "${files[@]}" | wmenu -i -l 20 -f "Terminess Nerd Font:size=16" -N "#202326" -n "#fcfcfc" -S "#ff0000" -s "#fcfcfc")
+    result=$(printf '%s\n' "${files[@]}" | wmenu -i -l 20 -f "Terminess Nerd Font:pixelsize=24" -N "#202326" -n "#fcfcfc" -S "#ff0000" -s "#fcfcfc")
 else
-    result=$(printf '%s\n' "${files[@]}" | dmenu -i -l 20 -fn "Terminess Nerd Font:size=16")
+    result=$(printf '%s\n' "${files[@]}" | dmenu -i -l 20 -fn "Terminess Nerd Font:pixelsize=24")
 fi
 
 [ -n "$result" ] && mpv --script=/usr/lib/mpv-mpris/mpris.so "$result"
